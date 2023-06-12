6a6c95c6b1cf949c0b9d04bbbbc0593a9d6eedd2
Anthony DINEL <anthony.dinel@gmail.com>
2023-06-12 15:56:07 +0200

    [BUGFIX] Back link to agenda

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,6 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'OpenAgenda',
     'description' => 'Display calendars from https://openagenda.com on your site.',
+	'version' => '1.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-12.4.99',
