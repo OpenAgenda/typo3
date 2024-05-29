754eed151c9787874e8dd2c4bedd7892bd72865f
Geoffroy Cochard <geoffroy.cochard@orleans-metropole.fr>
2024-05-29 14:19:23 +0200

    [oa-typo3] : path

--- Configuration/TypoScript/Config/view.ts
+++ Configuration/TypoScript/Config/view.ts
@@ -1,10 +1,10 @@
 plugin.tx_openagenda {
     view {
         templateRootPaths.0 = EXT:openagenda/Resources/Private/Templates/
-        templateRootPaths.5 = {$plugin.tx_openagenda.view.templateRootPath}
+        templateRootPaths.1 = {$plugin.tx_openagenda.view.templateRootPath}
         partialRootPaths.0 = EXT:openagenda/Resources/Private/Partials/
-        partialRootPaths.5 = {$plugin.tx_openagenda.view.partialRootPath}
+        partialRootPaths.1 = {$plugin.tx_openagenda.view.partialRootPath}
         layoutRootPaths.0 = EXT:openagenda/Resources/Private/Layouts/
-        layoutRootPaths.5 = {$plugin.tx_openagenda.view.layoutRootPath}
+        layoutRootPaths.1 = {$plugin.tx_openagenda.view.layoutRootPath}
     }
 }
