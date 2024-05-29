08c4da302f06d30a1c810470041804ec82091669
Geoffroy Cochard <geoffroycochard@gmail.com>
2024-05-29 10:18:45 +0200

    #1 Add / fix config template

--- Configuration/TypoScript/Config/view.ts
+++ Configuration/TypoScript/Config/view.ts
@@ -1,10 +1,10 @@
 plugin.tx_openagenda {
     view {
         templateRootPaths.0 = EXT:openagenda/Resources/Private/Templates/
-        templateRootPaths.1 = {$plugin.tx_openagenda.view.templateRootPath}
+        templateRootPaths.5 = {$plugin.tx_openagenda.view.templateRootPath}
         partialRootPaths.0 = EXT:openagenda/Resources/Private/Partials/
-        partialRootPaths.1 = {$plugin.tx_openagenda.view.partialRootPath}
+        partialRootPaths.5 = {$plugin.tx_openagenda.view.partialRootPath}
         layoutRootPaths.0 = EXT:openagenda/Resources/Private/Layouts/
-        layoutRootPaths.1 = {$plugin.tx_openagenda.view.layoutRootPath}
+        layoutRootPaths.5 = {$plugin.tx_openagenda.view.layoutRootPath}
     }
 }
