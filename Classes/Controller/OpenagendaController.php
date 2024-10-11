6be1b2c305cf873d035d546a6ca5dee686c1916e
Anthony DINEL <anthony.dinel@gmail.com>
2024-10-11 10:47:17 +0200

    [BUGFIX] Bug in Ajax Callback

--- Classes/Controller/OpenagendaController.php
+++ Classes/Controller/OpenagendaController.php
@@ -145,7 +145,7 @@ class OpenagendaController extends ActionController
 				$from,
 				$this->settings['calendarUid'],
 				$this->settings['language'],
-				null,
+				$this->openagendaHelper->getLanguageId(),
 				$filters);
         } else {
             $erreur = true;
@@ -396,7 +396,7 @@ class OpenagendaController extends ActionController
 					$queryInfo['settingsOpenagendaLanguage'],
 					$queryInfo['settingsOpenagendaLanguageId'],
 					$filters,
-					$queryInfo['settingsOpenagendaPage'],
+					null,
 					false,
 					false
 				);
